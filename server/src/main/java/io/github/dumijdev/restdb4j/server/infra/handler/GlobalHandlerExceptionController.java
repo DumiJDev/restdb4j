d122d46bedf4ea386d5ead8f1beb32097c87e387
Dumildes Paulo <dumildesfernando@outlook.com>
2024-12-28 21:27:44 +0100

    feat: add global exception handler

--- server/src/main/java/io/github/dumijdev/restdb4j/server/infra/handler/GlobalHandlerExceptionController.java
+++ server/src/main/java/io/github/dumijdev/restdb4j/server/infra/handler/GlobalHandlerExceptionController.java
@@ -2,11 +2,15 @@ package io.github.dumijdev.restdb4j.server.infra.handler;
 
 import io.github.dumijdev.restdb4j.server.application.core.domain.exceptions.InvalidException;
 import io.github.dumijdev.restdb4j.server.application.core.domain.exceptions.ValidationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalHandlerExceptionController extends ResponseEntityExceptionHandler {
@@ -15,5 +19,16 @@ public class GlobalHandlerExceptionController extends ResponseEntityExceptionHan
     var errors = ex.exceptions();
 
     return ResponseEntity.badRequest().body(errors);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+    var responseEntity =  ResponseEntity.internalServerError().headers(headers);
+
+    if (ex instanceof ValidationException ve) {
+      return responseEntity.body(ve.exceptions());
+    }
+
+    return responseEntity.body(List.of(new ValidationException.ItemException("internal", ex.getMessage())));
   }
 }
